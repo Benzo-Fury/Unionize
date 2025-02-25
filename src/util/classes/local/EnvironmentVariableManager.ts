aac2f5fc7d63d55dcd30d3ddc30088fd9adc22d9
Neo <80315475+Benzo-Fury@users.noreply.github.com>
2025-02-25 10:38:54 +1000

    fix: environment variables not loading

--- src/util/classes/local/EnvironmentVariableManager.ts
+++ src/util/classes/local/EnvironmentVariableManager.ts
@@ -2,13 +2,14 @@ import type { EnvironmentVariables } from "ts/interfaces/EnvironmentVariables";
 
 // todo: make it so the enviroment variables the code uses dont have "_FILE" and it has a map to resolve what vars are from a file.
 // todo: add support to load any defined variables in the actual enviroment instead of just secrets.
+// todo: add a development mode where secrets are loaded directly from files.
 
 const variableKeys = [
   "BOT_TOKEN_FILE",
   "N4J_AUTH_FILE",
   "MONGO_USERNAME_FILE",
-  "MONGO_PASSWORD_FILE"
-]
+  "MONGO_PASSWORD_FILE",
+];
 
 type EnvironmentVariable = keyof EnvironmentVariables;
 
@@ -27,16 +28,16 @@ export class EVM {
 
   public static async new() {
     const evm = new EVM();
-    for (const key in variableKeys) {
-      let value: string;
+    for (const key of variableKeys) {
+      let value = Bun.env[key]!;
 
       // Checking if key is actually a file
       if (key.endsWith("FILE")) {
         // Resolving if true
-        value = await evm.resolveSecret(key);
+        value = await evm.resolveSecret(value);
       }
 
-      evm.vars.set(key as EnvironmentVariable, value!);
+      evm.vars.set(key as EnvironmentVariable, value);
     }
 
     return evm;
