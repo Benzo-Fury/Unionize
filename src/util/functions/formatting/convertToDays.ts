5dd431e0d0fe5459fc13970c62df97022a83ff67
Neo <80315475+Benzo-Fury@users.noreply.github.com>
2025-06-04 19:19:55 +1000

    docs(core): add missing typedocs

--- src/util/functions/formatting/convertToDays.ts
+++ src/util/functions/formatting/convertToDays.ts
@@ -1,4 +1,6 @@
+/**
+ * Converts a duration in milliseconds to the equivalent number of days.
+ */
 export default function (durationInMillis: number) {
-
-    return Math.ceil(durationInMillis / (24 * 60 * 60 * 1000))
+  return Math.ceil(durationInMillis / (24 * 60 * 60 * 1000));
 }
--- src/util/functions/formatting/daysToMilli.ts
+++ src/util/functions/formatting/daysToMilli.ts
@@ -1,3 +1,4 @@
+/** Converts a number of days into milliseconds. */
 export function daysToMilli(days: number) {
   return days * 24 * 60 * 60 * 1000;
 }
